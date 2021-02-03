8b195338ff96691418532f5a2db18f79cc06231b
jinnzy <839444083@qq.com>
2021-02-03 22:38:43 +0800

    更新配置

--- setup_env.sh
+++ setup_env.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 #set -u -e -o pipefail
 
-#export NOTION_TOKEN=d1ab20cd92feb723f825d091fbedbc370e75ed12e795d8bc1a5315eb985b8ac2a2c0fd8394a4ffda0ec8b4a8efa1ddd1e58cc908da49ac7ee853066f3c80e82fe24ef4ce9de2ff4a754e06d0a9e4
-## setup git
-#git config user.email "839444083@qq.com"
-#git config user.name "jinnzy"
+export NOTION_TOKEN=d1ab20cd92feb723f825d091fbedbc370e75ed12e795d8bc1a5315eb985b8ac2a2c0fd8394a4ffda0ec8b4a8efa1ddd1e58cc908da49ac7ee853066f3c80e82fe24ef4ce9de2ff4a754e06d0a9e4
+# setup git
+git config user.email "839444083@qq.com"
+git config user.name "jinnzy"
 #
 #
-#git clone git@github.com:jinnzy/jinnzy.github.io.source.git blog
-#./NotiGoCMS
+git clone git@github.com:jinnzy/jinnzy.github.io.source.git blog
+./NotiGoCMS
 
-#cd blog
-#git status
-#git add *
+cd blog
+git status
+git add *
 #git status
 #now=`date "+%Y-%m-%d %a"`
-
+git commit -m "update"
+git push origin main
 # "git commit" returns 1 if there's nothing to commit, so don't report this as failed build
 #set +e
 #git commit -am "ci: update from notion on ${now}"
